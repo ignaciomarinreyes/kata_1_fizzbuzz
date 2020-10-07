e2268b4fb715ff81938e1b394368253ccd263bf4
ignaciomarinreyes <ignaciomarinreyes@gmail.com>
2020-10-07 11:51:36 +0100

    First test, refactor 2

--- src/test/java/test/FizzBuzz_.java
+++ src/test/java/test/FizzBuzz_.java
@@ -3,7 +3,7 @@ package test;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class FizzBuzz_ {
     // Multiplos 3 Fizz, múltiplos 5 Buzz, múltiplos de 3 y 5, FizzBuzz
@@ -18,12 +18,13 @@ public class FizzBuzz_ {
 
     @Test
     public void should_return_number_when_it_is_not_fizz_and_not_buzz() {
-        assertEquals("1", fizzBuzz.get(1));
+        assertThat(fizzBuzz.of(1)).isEqualTo("1");
+        assertThat(fizzBuzz.of(2)).isEqualTo("2");
     }
 
     public class FizzBuzz {
-        public String get(int number) {
-            return "1";
+        public String of(int number) {
+            return String.valueOf(number);
         }
     }
 }
